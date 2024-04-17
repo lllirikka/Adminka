97ec4985bf809c8dc3347c418e6a5e459b5c8fa1
lllirikka <kirillsovetov04@gmail.com>
2024-04-17 23:14:37 +0500

    -3

--- routes/games.js
+++ routes/games.js
@@ -2,6 +2,12 @@ const gamesRouter = require("express").Router();
 
 const getAllGames = async (req, res) => {};
 
+const deleteGame = async (req, res) => {};
+
+const addGame = async (req, res) => {};
+
 gamesRouter.get("/games", getAllGames);
+gamesRouter.delete("/games/:1", deleteGame);
+gamesRouter.post("/games", addGame);
 
 module.exports = gamesRouter;
